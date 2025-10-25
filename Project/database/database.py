b505fe948a468f45968032aa10e2d7f0558cffbe
draqa214 <131980011+draqa214@users.noreply.github.com>
2025-10-25 13:16:24 -0400

    Initial Setup of database and APIs

---
+++ Project/database/database.py
@@ -0,0 +1,13 @@
+import os
+import databases
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+print(DATABASE_URL)
+database = databases.Database(
+    DATABASE_URL, min_size=5, max_size=20, ssl=False, statement_cache_size=0
+)
