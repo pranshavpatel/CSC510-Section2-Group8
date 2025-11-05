41fc8b0124e8b9e3496056c7344652532ee6e13f
Namit2003 <namit.k.patel@gmail.com>
2025-11-04 21:29:23 -0500

    removed unnecessary code from home page and spotify router added to the main server

--- Project/app/main.py
+++ Project/app/main.py
@@ -3,6 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 from .config import settings
 from .routers import meals, catalog, orders, debug_auth, auth_routes, me, address, cart
 import sys
+from Mood2FoodRecSys.Spotify_Auth import router as spotify_router
+from database.database import database
 
 app = FastAPI(title="VibeDish API", version="0.1.0")
 
@@ -27,3 +29,4 @@ app.include_router(meals.router, prefix="/meals", tags=["meals"])
 app.include_router(catalog.router, prefix="/catalog", tags=["catalog"])
 app.include_router(orders.router, prefix="/orders", tags=["orders"])
 app.include_router(debug_auth.router, prefix="/debug", tags=["debug"])
+app.include_router(spotify_router)
