291810cdc883137ee4793a09e254bb635aa2f5d1
Pranshav Patel <96743783+pranshavpatel@users.noreply.github.com>
2025-11-07 04:11:53 -0500

    fixed database issues

--- Project/database/database.py
+++ Project/database/database.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL") or "postgresql://test:test@localhost:5432/test"
 database = databases.Database(
     DATABASE_URL, min_size=5, max_size=20, ssl=False, statement_cache_size=0
 )
